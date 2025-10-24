8749c37b88d3a060475b16f268eebcafcf37b9e2
Israel Trujillo <it231425@gmail.com>
2025-10-23 19:41:34 -0500

    modificacion de los files admin apps y models de Career

--- Universidad/Aplicaciones/Career/models.py
+++ Universidad/Aplicaciones/Career/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from Aplicaciones.Faculty.models import Facultys
 
-# Create your models here.
+class Careers(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    facultad = models.ForeignKey( Facultys, on_delete = models.CASCADE )
+
+    name_career = models.CharField(max_length=150)
+    directory_car = models.CharField(max_length=15, null=True, blank=True)
+    duration_years = models.IntegerField(null=True, blank=True)   
+    phone_car = models.CharField(max_length=15, null=True, blank=True)
+    email_car = models.CharField(max_length=50, null=True, blank=True)
+
+    # Archivos (como en tu ejemplo)
+    logo = models.FileField(
+        upload_to='cargos',  
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        # Muestra el nombre de la carrera y la facultad
+        return f"{self.name_career} - {self.facultad.nombre_facultad}"
